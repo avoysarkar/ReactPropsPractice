459764823a0fc29477ef2bbebbfab16e38e6811c
avoysarkar <73129752+avoysarkar@users.noreply.github.com>
2021-08-03 23:41:51 +0530

    React Props usage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,28 @@
 import React from "react";
+import Card from "./Card";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        web="b@beyonce.com"
+      />
+      <Card
+        name="Jack Bauer"
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        tel="+987 654 321"
+        web="jack@nowhere.com"
+      />
+      <Card
+        name="Chuck Norris"
+        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        tel="+918 372 574"
+        web="gmail@chucknorris.com"
+      />
     </div>
   );
 }
